6434ead36323b5eff78d8be30c3a89c322b50b83
amalms-ai <amalms8590@gmail.com>
2026-09-11 23:10:30 +0530

    Day 1: Create Python IoT simulator

---
+++ src/simulator/iot_simulator.py
@@ -0,0 +1,25 @@
+import random
+from datetime import datetime
+
+
+def generate_sensor_data():
+    container_id = f"CONT_{random.randint(1, 10):03d}"
+
+    temperature = round(random.uniform(2, 10), 2)
+    humidity = round(random.uniform(60, 90), 2)
+    vibration = round(random.uniform(0, 1), 2)
+
+    sensor_data = {
+        "container_id": container_id,
+        "timestamp": datetime.now().isoformat(),
+        "temperature": temperature,
+        "humidity": humidity,
+        "vibration": vibration
+    }
+
+    return sensor_data
+
+
+if __name__ == "__main__":
+    data = generate_sensor_data()
+    print(data)
